dcb426948e89baac759c5c76edb87f0034663a89
SeoungHyuckWon <66349040+SeoungHyuckWon@users.noreply.github.com>
2021-03-23 02:25:29 +0900

    Update food.py
    foodInput함수 foodOutput함수 추가

--- src/food.py
+++ src/food.py
@@ -1,11 +1,66 @@
+import time
+import string
+import os
+from inputCheck import inputFoodName, checkListNum, inputFoodAmount, inputFoodExpiration, matchFoodAmount
 
+def foodInput(path):
+    while True:
+        print("식품명, 양, 유통기한(YYYYMMDD)를 공백을 기준으로 입력하세요.")
+        string = input()
+        input_string = string.split()
+        if inputFoodName(input_string[0]) and checkListNum(input_string,3) and inputFoodAmount(input_string[1]) and inputFoodExpiration(input_string[2]):
+            while True:
+                title = time.strftime('%Y%m%d%I%M%S',time.localtime())
+                if os.path.isfile("./refrigerator/"+path+"/"+title+".txt") == True:
+                    time.sleep(1)
+                else:
+                    break
+            f = open("./refrigerator/"+path+"/"+title+".txt", 'w',encoding='utf-8')
+            f.write(string)
+            f.close()
+            print("식품 입고 완료")
+            break
 
-def foodInput():
-    print("식품 입고를 위한 함수")
+def foodOutput(path):
+    file_list = os.listdir("./refrigerator/"+path)
+    file_list_txt = [file for file in file_list if file.endswith(".txt")]
+    expiration_date = 99999999
+    food_file = ""
+    while True:
+        print("식품명, 양을 공백을 기준으로 입력하세요.")
+        string = input()
+        input_string = string.split()
+        if inputFoodName(input_string[0]) and checkListNum(input_string,2) and inputFoodAmount(input_string[1]):
+            food_name = input_string[0]
+            out_food_amount = input_string[1]
+            for i in file_list_txt:
+                f = open("./refrigerator/"+path+"/"+i,'r',encoding='utf-8')
+                line = f.readline()
+                buf = line.split()
+                if buf[0] == food_name and expiration_date > int(buf[2]):
+                    food_file = i
+                    food_amount = buf[1]
+                    expiration_date = int(buf[2])
+                f.close()
+            break
+    if food_file == "":
+        print("식품이 존재하지 않습니다.")
+    else:
+        if matchFoodAmount(food_amount,out_food_amount):
+            amount = matchFoodAmount(food_amount,out_food_amount)
+            if amount[0] > amount[1]:
+                amount[0] = amount[0] - amount[1]
+                new_line = food_name + " " + str(amount[0])+amount[2]+ " " + str(expiration_date)
+                f = open("./refrigerator/"+path+"/"+food_file,'w',encoding='utf-8')
+                f.write(new_line)
+                f.close()
+                print(food_name+"을 "+str(amount[1])+"만큼 출고하여 "+str(amount[0])+"만큼 남음")
+            elif amount[0] == amount[1]:
+                os.remove("./refrigerator/"+path+"/"+food_file)
+                print(food_name+"전부 출고 완료")
+            else:
+                print(food_name+"의 최대 출고 가능한 양은 "+str(amount[0])+amount[2]+" 입니다.")
 
-
-def foodOutput():
-    print("식품 출고를 위한 함수")
 
 
 def lessExpirationDate():
