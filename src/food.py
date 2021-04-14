741476a7f6164b9a7386f4c285ecf044b5a2a8c6
SeoungHyuckWon <66349040+SeoungHyuckWon@users.noreply.github.com>
2021-04-14 15:09:09 +0900

    식품 입고 출고 수정

--- src/food.py
+++ src/food.py
@@ -4,14 +4,14 @@ import os
 from inputCheck import inputFoodName, checkListNum, inputFoodAmount, inputFoodExpiration, matchFoodAmount
 
 def foodInput(path):
+    print("식품명, 식품의 양, 식품의 유통기한(YYYYMMDD)을 공백을 기준으로 입력하세요.")
     while True:
-        print("식품명, 식품의 양, 식품의 유통기한(YYYYMMDD)을 공백을 기준으로 입력하세요.")
         string = input("입력 > ")
         try:
             input_string = string.split()
             if inputFoodName(input_string[0]) and checkListNum(input_string,3) and inputFoodAmount(input_string[1]) and inputFoodExpiration(input_string[2]):
                 while True:
-                    title = time.strftime('%Y%m%d%I%M%S',time.localtime())
+                    title = time.strftime('%Y%m%d%H%M%S',time.localtime())
                     if os.path.isfile("./refrigerator/"+path+"/"+title+".txt") == True:
                         time.sleep(1)
                     else:
@@ -22,15 +22,15 @@ def foodInput(path):
                 print("식품 입고 완료")
                 break
         except:
-            pass
+            print("공백을 기준으로 조건에 맞게 3가지 요소만 입력하세요.")
 
 def foodOutput(path):
     file_list = os.listdir("./refrigerator/"+path)
     file_list_txt = [file for file in file_list if file.endswith(".txt")]
     expiration_date = 99999999
     food_file = ""
+    print("식품명, 양을 공백을 기준으로 입력하세요.")
     while True:
-        print("식품명, 양을 공백을 기준으로 입력하세요.")
         string = input("입력 > ")
         try:
             input_string = string.split()
@@ -46,26 +46,29 @@ def foodOutput(path):
                         food_amount = buf[1]
                         expiration_date = int(buf[2])
                     f.close()
-                break
         except:
-            pass
-    if food_file == "":
-        print("식품이 존재하지 않습니다.")
-    else:
-        if matchFoodAmount(food_amount,out_food_amount):
-            amount = matchFoodAmount(food_amount,out_food_amount)
-            if amount[0] > amount[1]:
-                amount[0] = amount[0] - amount[1]
-                new_line = food_name + " " + str(amount[0])+amount[2]+ " " + str(expiration_date)
-                f = open("./refrigerator/"+path+"/"+food_file,'w',encoding='utf-8')
-                f.write(new_line)
-                f.close()
-                print(food_name+"을(를) "+str(amount[1])+str(amount[2])+" 만큼 출고하여 "+str(amount[0])+amount[2]+" 만큼 남음")
-            elif amount[0] == amount[1]:
-                os.remove("./refrigerator/"+path+"/"+food_file)
-                print(food_name+"을(를) 전부 출고 완료")
-            else:
-                print(food_name+"의 최대 출고 가능한 양은 "+str(amount[0])+amount[2]+" 입니다.")
+            print("공백을 기준으로 조건에 맞게 2가지 요소만 입력하세요.")
+        if food_file == "":
+            print("식품이 존재하지 않습니다.")
+            break
+        else:
+            if matchFoodAmount(food_amount,out_food_amount):
+                amount = matchFoodAmount(food_amount,out_food_amount)
+                if amount[0] > amount[1]:
+                    amount[0] = amount[0] - amount[1]
+                    new_line = food_name + " " + str(amount[0])+amount[2]+ " " + str(expiration_date)
+                    f = open("./refrigerator/"+path+"/"+food_file,'w',encoding='utf-8')
+                    f.write(new_line)
+                    f.close()
+                    print(food_name+"을(를) "+str(amount[1])+str(amount[2])+" 만큼 출고하여 "+str(amount[0])+amount[2]+" 만큼 남음")
+                    break
+                elif amount[0] == amount[1]:
+                    os.remove("./refrigerator/"+path+"/"+food_file)
+                    print(food_name+"을(를) 전부 출고 완료")
+                    break
+                else:
+                    print(food_name+"의 최대 출고 가능한 양은 "+str(amount[0])+amount[2]+" 입니다.")
+                    break
 
 
 
