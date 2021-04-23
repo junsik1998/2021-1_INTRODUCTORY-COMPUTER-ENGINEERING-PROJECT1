aa69a604e02d60d61cf151466da995139c8c8bf7
Woosu37564 <80671415+Woosu37564@users.noreply.github.com>
2021-04-24 01:03:47 +0900

    Update showList.py

--- src/showList.py
+++ src/showList.py
@@ -12,42 +12,51 @@ def showRecipeList(menuName,recipe_list):
     return menu
 
 
+
 def showALLFood(path):
-    food_list=["메인메뉴로 돌아가기"]
+    food_list=[]
     index_list=[]
     real_list=[]
+
     print("정렬기준과 오름/내림차순을 공백을 기준으로 입력하시오.")
     while(True):
         string=input("입력> ")
-        input_string=string.split()
-        if checkListNum(input_string,2) and inputConditionName(input_string[0],input_string[1]):
+        input_condition=string.split()
+        if checkListNum(input_condition,2) and inputConditionName(input_condition[0],input_condition[1]):
             break
-    file_list = os.listdir("./refrigerator/"+path)
-    file_list_txt = [file for file in file_list if file.endswith(".txt")]
-    for i in file_list_txt:
-        f = open("./refrigerator/"+path+"/"+i,'r',encoding='utf-8')
-        line = f.readline()
-        buf = line.split()        
-        real_list.append([buf[0],buf[1],buf[2],i])
-        f.close()
 
-    if input_string[0]=="식품명":
-        real_list.sort()
-    if input_string[0]=="유통기한"and input_string[1]=='오름차순':
-        real_list.sort(key=lambda x : x[2])
-    if input_string[0]=="유통기한"and input_string[1]=='내림차순':
-        real_list.sort(key=lambda x : x[2], reverse=True)
-    if input_string[0]=="식품생성날짜"and input_string[1]=='오름차순':
-        real_list.sort(key=lambda x : x[3])
-    if input_string[0]=="식품생성날짜"and input_string[1]=='내림차순':
-        real_list.sort(key=lambda x : x[3], reverse=True)
-
-    i=0
-    while i<len(real_list):
-        food_list.append(real_list[i][0])
-        index_list.append(real_list[i][3])
-        i+=1
     while(True):
+        food_list.clear()
+        index_list.clear()
+        real_list.clear()
+        food_list.append("메인메뉴로 돌아가기")
+        file_list = os.listdir("./refrigerator/"+path)
+        file_list_txt = [file for file in file_list if file.endswith(".txt")]
+        i=0
+        for i in file_list_txt:
+            f = open("./refrigerator/"+path+"/"+i,'r',encoding='utf-8')
+            line = f.readline()
+            buf = line.split()        
+            real_list.append([buf[0],buf[2],i])
+            f.close()
+
+        if input_condition[0]=="식품명":
+            real_list.sort()
+        if input_condition[0]=="유통기한"and input_condition[1]=='오름차순':
+            real_list.sort(key=lambda x : x[1])
+        if input_condition[0]=="유통기한"and input_condition[1]=='내림차순':
+            real_list.sort(key=lambda x : x[1], reverse=True)
+        if input_condition[0]=="식품생성날짜"and input_condition[1]=='오름차순':
+            real_list.sort(key=lambda x : x[2])
+        if input_condition[0]=="식품생성날짜"and input_condition[1]=='내림차순':
+            real_list.sort(key=lambda x : x[2], reverse=True)
+        i=0
+        while i<len(real_list):
+            food_list.append(real_list[i][0])
+            index_list.append(real_list[i][2])
+            i+=1
+
+
         menu=menuSelect("식품목록",food_list)
         if(menu==0):
             break
@@ -77,6 +86,7 @@ def showALLFood(path):
                             f.write(string)
                             f.close()
                             break
+
                 if(submenu==2):
                     os.remove("./refrigerator/"+path+"/"+index_list[menu-1])
                     food_list.pop(menu)
@@ -84,3 +94,10 @@ def showALLFood(path):
                     print("삭제가 완료되었습니다.")
                     break
         
+    
+
+    
+
+        
+    
+
